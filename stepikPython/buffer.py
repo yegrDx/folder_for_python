c737e44c74a65b7c73631a3249728bc64c3f6414
Yegor Popov <yegor_popov04@mail.ru>
2024-03-05 21:44:50 +0300

    buffer1.0

---
+++ stepikPython/buffer.py
@@ -0,0 +1,24 @@
+class Buffer:
+
+    def __init__(self):
+        self.values = []
+    def add(self, *a):
+
+        for i in a:
+            self.values.append(i)
+            if len(self.values) >= 5:
+                print(sum(self.values))
+                self.values.clear()
+
+    def get_current_part(self):
+        return self.values
+
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part() # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+buf.get_current_part() # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+buf.get_current_part() # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+buf.get_current_part() # вернуть [1]
