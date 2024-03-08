49a2db150f6c4c4474fcaab4fc8b6c5232c5ed4f
Yegor Popov <yegor_popov04@mail.ru>
2024-03-08 13:03:33 +0300

    stack1.0

---
+++ stepikPython/stack.py
@@ -0,0 +1,20 @@
+class ExtendedStack(list):
+    def sum(self):
+        top1 = self.pop()
+        top2 = self.pop()
+        self.append(top1 + top2)
+
+    def sub(self):
+        top1 = self.pop()
+        top2 = self.pop()
+        self.append(top1 - top2)
+
+    def mul(self):
+        top1 = self.pop()
+        top2 = self.pop()
+        self.append(top1 * top2)
+
+    def div(self):
+        top1 = self.pop()
+        top2 = self.pop()
+        self.append(top1 // top2)
